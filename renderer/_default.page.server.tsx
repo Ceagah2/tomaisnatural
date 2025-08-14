8f3114313decef1c34b754453153005e2ff0a50b
Ceagah <carlosceagah@gmail.com>
2025-08-14 15:23:20 -0300

    feat: change head meta data

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -1,16 +1,14 @@
 export { render }
-// See https://vite-plugin-ssr.com/data-fetching
 export const passToClient = ['pageProps', 'urlPathname']
 
 import ReactDOMServer from 'react-dom/server'
+import { dangerouslySkipEscape, escapeInject } from 'vite-plugin-ssr/server'
+import Icon from '../assets/icons/favicon.ico'
 import { PageShell } from './PageShell'
-import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
-import logoUrl from './logo.svg'
 import type { PageContextServer } from './types'
 
 async function render(pageContext: PageContextServer) {
   const { Page, pageProps } = pageContext
-  // This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
   if (!Page) throw new Error('My render() hook expects pageContext.Page to be defined')
   const pageHtml = ReactDOMServer.renderToString(
     <PageShell pageContext={pageContext}>
@@ -18,16 +16,14 @@ async function render(pageContext: PageContextServer) {
     </PageShell>
   )
 
-  // See https://vite-plugin-ssr.com/head
-  const { documentProps } = pageContext.exports
-  const title = (documentProps && documentProps.title) || 'Vite SSR app'
-  const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr'
+  const title = 'To + Natural || Produtos Naturais e Orgânicos'
+  const desc = "Descubra o melhor da natureza com To + Natural, sua loja de produtos naturais e orgânicos. Oferecemos uma seleção cuidadosa de alimentos saudáveis, cosméticos sustentáveis e muito mais. Experimente a pureza e qualidade dos nossos produtos, feitos para cuidar de você e do planeta. Navegue pelo nosso site e transforme sua rotina com escolhas conscientes e saudáveis."
 
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
+        <link rel="icon" href="${Icon}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
@@ -35,12 +31,10 @@ async function render(pageContext: PageContextServer) {
       <body>
         <div id="react-root">${dangerouslySkipEscape(pageHtml)}</div>
       </body>
-    </html>`
+    </html>`;
 
   return {
     documentHtml,
-    pageContext: {
-      // We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
-    }
+    pageContext: {}
   }
 }

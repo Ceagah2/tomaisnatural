ed7a34dd99e2150243899df41775f339e7101956
Carlos Ceagah <carlosceagah@gmail.com>
2025-08-14 19:38:45 -0300

    chore: update vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import ssr from 'vite-plugin-ssr/plugin';
 
 
 export default defineConfig({
   plugins: [react(), ssr(), tailwindcss()],
-})
+  build: {
+    ssr: false,
+    outDir: "dist",
+  },
+});

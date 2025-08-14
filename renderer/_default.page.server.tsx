3884b2122f248d56a2d674ec5893f40f80b0119d
Carlos Ceagah <carlosceagah@gmail.com>
2025-08-14 18:57:35 -0300

    refactor: remove favicon import to use it right on proper place

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -3,7 +3,6 @@ export const passToClient = ['pageProps', 'urlPathname']
 
 import ReactDOMServer from 'react-dom/server'
 import { dangerouslySkipEscape, escapeInject } from 'vite-plugin-ssr/server'
-import Icon from '../assets/icons/favicon.ico'
 import { PageShell } from './PageShell'
 import type { PageContextServer } from './types'
 
@@ -23,7 +22,7 @@ async function render(pageContext: PageContextServer) {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${Icon}" />
+        <link rel="icon" href="../assets/icons/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
